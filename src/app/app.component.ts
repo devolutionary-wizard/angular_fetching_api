f5e2d832c7b06c33f6592f1aefd9f5bd25d87a6f
SEANGLAY <liseanglay@gmail.com>
2023-05-05 09:49:41 +0700

    initiate project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServicesService } from './services/api-services.service';
+import { Todo } from './shared/models/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit   {
   title = 'angular_fetching_api';
+  newData: Todo[] = [];
+
+  constructor(private _apiService: ApiServicesService){}
+  
+  ngOnInit(): void {
+      this._apiService.getData().subscribe(res=>{
+        this.newData=res
+      })    
+  }
 }
